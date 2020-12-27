b730ff3cfc04f0e57ca74cd49fd9641121e1816e
kevinutapgithub <69888548+kevinutapgithub@users.noreply.github.com>
2020-12-27 09:22:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -55,11 +55,13 @@ var map = new ol.Map({
 	  style: pointStyleFunction
     })
   ],
-  view: new ol.View({
+  view: view
+});
+
+var view=new ol.View({
     center: ol.proj.fromLonLat([10.6,60.05]),
     zoom: 10
-  })
-});
+  });
 
 function onClick(e) {
   var features = map.getFeaturesAtPixel(e.pixel);
