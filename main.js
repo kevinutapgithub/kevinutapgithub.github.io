65dea1860acce15de80fc82ead9da72163dbc748
kevinutapgithub <69888548+kevinutapgithub@users.noreply.github.com>
2021-01-10 16:47:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -69,3 +69,59 @@ function onClick(e) {
   }
 }
 map.on("click", onClick);
+
+          var geolocation = new ol.Geolocation({
+            // enableHighAccuracy must be set to true to have the heading value.
+            trackingOptions: {
+              enableHighAccuracy: true
+            },
+			tracking: true,
+            projection: map.getView().getProjection()
+          });
+
+          function el(id) {
+            return document.getElementById(id);
+          }
+
+          el('track').addEventListener('change', function() {
+            geolocation.setTracking(this.checked);
+          });
+
+          // handle geolocation error.
+          geolocation.on('error', function(error) {
+            var info = document.getElementById('info');
+            info.innerHTML = error.message;
+            info.style.display = '';
+          });
+
+          var accuracyFeature = new ol.Feature();
+          geolocation.on('change:accuracyGeometry', function() {
+            //accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+          });
+
+          var positionFeature = new ol.Feature();
+          positionFeature.setStyle(new ol.style.Style({
+            image: new ol.style.Circle({
+              radius: 6,
+              fill: new ol.style.Fill({
+                color: '#ff00aa'
+              }),
+              stroke: new ol.style.Stroke({
+                color: '#fff',
+                width: 2
+              })
+            })
+          }));
+
+          geolocation.on('change:position', function() {
+            var coordinates = geolocation.getPosition();
+            positionFeature.setGeometry(coordinates ?
+              new ol.geom.Point(coordinates) : null);
+          });
+
+          new ol.layer.Vector({
+            map: map,
+            source: new ol.source.Vector({
+              features: [accuracyFeature, positionFeature]
+            })
+          });
